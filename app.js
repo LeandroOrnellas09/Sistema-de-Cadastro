413ab7a5bbf609a8f6ccd96784cc77bd5c29a8c3
Leandro_Ornellas <leandro.ocl2@gmail.com>
2023-08-29 22:51:43 -0300

    voltando atras

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está conectando ao bando de dados ")
+console.log("Acabei de fazer uma alteração")
 console.log("Mudança")
