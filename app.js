bbd2bc45fbd80818f65a6485e183729387a15fc0
LeandroOrnellas09 <142859024+LeandroOrnellas09@users.noreply.github.com>
2023-08-29 22:08:38 -0300

    alteração

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está conectando ao bando de dados ")
-console.log("Acabei de fazer uma alteração")
-console.log("Mudança")
+
